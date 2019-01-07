84a8b02b8c87499e9e2147da9f25a307e0c21c7f
Sara King <sara@sara-king.com>
2019-01-06 19:32:12 -0800

    colors ramdomized with new item

--- trends-visualizer/src/App.js
+++ trends-visualizer/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const colors = ["orange", "CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black", "orange", "CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black", "orange", "CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black", "orange", "CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black"];
+const colors = ["orange", "CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black", "orange", "CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black","orange", "CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black"];
 var names = ["mandrill","ground hog","chimpanzee","squirrel","panther","warthog","kitten","wolf","monkey","ferret","anteater","coyote","polar bear","zebra","mountain goat","vicuna","horse","dromedary","puma","ape","canary","steer","guinea pig","wombat","opossum","deer","kangaroo","jaguar","seal","okapi","leopard","walrus","gorilla","crocodile","eagle owl","thorny devil","parrot","chameleon","budgerigar","lamb","alpaca","dingo","eland","baboon","mongoose","donkey","cheetah","camel","marmoset","chipmunk","antelope","beaver","silver fox","capybara","lovebird","aardvark","ibex","dog","waterbuck","bald eagle","cat","gila monster","duckbill platypus","buffalo","starfish","pronghorn","mouse","orangutan","shrew","ocelot","weasel","bear","chinchilla","lemur","musk-ox","hartebeest","grizzly bear","dormouse","gemsbok","meerkat","bumble bee","doe","fox","marten","llama","argali","porpoise","bat","finch","chamois","bull","lizard","coati","fawn","hyena","parakeet","yak","pony","woodchuck","peccary","cow","ewe","ox","ermine","skunk","goat","sheep","raccoon","rooster","salamander","hamster","alligator","colt","bison","frog","giraffe","hedgehog","crow","guanaco","jackal","rat","lion","gnu","rhinoceros","puppy","gazelle","bighorn","mule","mink","panda","octopus","aoudad","basilisk","badger","ram","lynx","prairie dog","dugong","elk","snake","porcupine","springbok","burro","mustang","oryx","otter","pig","blue crab","stallion","gopher","impala","bunny","dung beetle","fish","tiger","hog","wildcat","mole","tapir","hare","turtle","addax","quagga","koala","civet","iguana","jerboa","snowy owl","muskrat","rabbit","mare","elephant","hippopotamus","wolverine","boar","cougar","zebu","mynah bird","whale","musk deer","reindeer","highland cow","chicken","armadillo","sloth","newt","moose","toad"];
 
 class App extends Component {
@@ -20,7 +20,8 @@ class App extends Component {
    this.handleShowLessBoxes = this.handleShowLessBoxes.bind(this);
    this.onUpdateItem = this.onUpdateItem.bind(this);
    this.colorStyle = this.colorStyle.bind(this);
-   this.newcolorStyleArray = this.newcolorStyleArray.bind(this);
+   this.newcolorStyleArray = this.newcolorStyleArray.bind(this); 
+   this.updateBackgroundColor = this.updateBackgroundColor.bind(this);
  }
 
  componentDidMount() {
@@ -76,7 +77,6 @@ handleShowLessColumns() {
 }
 
 newcolorStyleArray(a){
-let { newColorsArray } = this.state;
 let j, x, i;
   for (i = a.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
@@ -94,16 +94,26 @@ colorStyle(index){
   return newColorsArray[index];
 }
 
+updateBackgroundColor(newItemIndex){
+  const newColor = colors[Math.floor(Math.random()*colors.length)];
+  let newColorsArray = [...this.state.newColorsArray]
+  newColorsArray[newItemIndex] = newColor
+  this.setState({
+    newColorsArray 
+  })
+}
+
 onUpdateItem = () => {
   let { showItems, names } = this.state;
   const itemArray = names.slice(0, showItems);
-  let replacedItem = itemArray[Math.floor(Math.random()*itemArray.length)];
+  let newItemIndex = Math.floor(Math.random()*itemArray.length);
+  let replacedItem = itemArray[newItemIndex];
   let newItem = names[Math.floor(Math.random()*names.length)];
+  this.updateBackgroundColor(newItemIndex)
   this.setState(state => {
      names = names.map((item) => {
       if(item === replacedItem){
         return replacedItem = newItem;
-        //update background color
       } else {
         return item;
       }
