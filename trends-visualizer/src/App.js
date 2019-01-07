91adb5faa88a6bf97781e78a051c89b21ac3e2c1
Sara King <sara@sara-king.com>
2019-01-06 17:12:00 -0800

    misc syntax editing

--- trends-visualizer/src/App.js
+++ trends-visualizer/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-//do eslint
 
 const colors = ["CornflowerBlue", "Aquamarine", "gray", "pink", "DarkCyan ", "CadetBlue", "silver", "salmon", "black"];
 var names = ["mandrill","ground hog","chimpanzee","squirrel","panther","warthog","kitten","wolf","monkey","ferret","anteater","coyote","polar bear","zebra","mountain goat","vicuna","horse","dromedary","puma","ape","canary","steer","guinea pig","wombat","opossum","deer","kangaroo","jaguar","seal","okapi","leopard","walrus","gorilla","crocodile","eagle owl","thorny devil","parrot","chameleon","budgerigar","lamb","alpaca","dingo","eland","baboon","mongoose","donkey","cheetah","camel","marmoset","chipmunk","antelope","beaver","silver fox","capybara","lovebird","aardvark","ibex","dog","waterbuck","bald eagle","cat","gila monster","duckbill platypus","buffalo","starfish","pronghorn","mouse","orangutan","shrew","ocelot","weasel","bear","chinchilla","lemur","musk-ox","hartebeest","grizzly bear","dormouse","gemsbok","meerkat","bumble bee","doe","fox","marten","llama","argali","porpoise","bat","finch","chamois","bull","lizard","coati","fawn","hyena","parakeet","yak","pony","woodchuck","peccary","cow","ewe","ox","ermine","skunk","goat","sheep","raccoon","rooster","salamander","hamster","alligator","colt","bison","frog","giraffe","hedgehog","crow","guanaco","jackal","rat","lion","gnu","rhinoceros","puppy","gazelle","bighorn","mule","mink","panda","octopus","aoudad","basilisk","badger","ram","lynx","prairie dog","dugong","elk","snake","porcupine","springbok","burro","mustang","oryx","otter","pig","blue crab","stallion","gopher","impala","bunny","dung beetle","fish","tiger","hog","wildcat","mole","tapir","hare","turtle","addax","quagga","koala","civet","iguana","jerboa","snowy owl","muskrat","rabbit","mare","elephant","hippopotamus","wolverine","boar","cougar","zebu","mynah bird","whale","musk deer","reindeer","highland cow","chicken","armadillo","sloth","newt","moose","toad"];
@@ -26,6 +25,7 @@ class App extends Component {
   const rand = Math.round(Math.random() * (3000 - 500)) + 300;
   this.interval = setInterval(() => this.onUpdateItem(), rand);
 }
+
 componentWillUnmount() {
   clearInterval(this.interval);
 }
@@ -71,22 +71,10 @@ handleShowLessColumns() {
     })
   }
 }
-  //randomize
-  //push to a used grid
-  //https://stackoverflow.com/questions/22467842/assign-random-picked-color-with-javascript
+
  colorStyle(index){
-  // let usedColors = [];
-  // color = colors [Math.floor(Math.random()*colors.length)];
-  // usedColors.push(color);
-  // let lastItem = usedColors.pop();
-
-  // while(lastItem === color){
-  //   color = colors [Math.floor(Math.random()*colors.length)];
-  // }
-return colors[index];
-
+  return colors[index];
 }
-
 
 onUpdateItem = () => {
   let { showItems, names } = this.state;
@@ -97,6 +85,7 @@ onUpdateItem = () => {
      names = names.map((item) => {
       if(item === replacedItem){
         return replacedItem = newItem;
+        //update background color
       } else {
         return item;
       }
